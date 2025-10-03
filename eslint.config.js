039b9176d501dca8287909860eb6f5cffcb0082f
AliAlabdouli011 <albdwlyly07@gmail.com>
2025-10-03 10:00:02 +1000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
-// eslint.config.js
-import js from "@eslint/js";
+// Flat config for ESLint v9 (CommonJS project + Jest)
+const js = require("@eslint/js");
+const globals = require("globals");
 
-export default [
-  js.configs.recommended,
+module.exports = [
   {
     files: ["**/*.js"],
+    ignores: ["node_modules/**", "coverage/**", "dist/**", "build/**"],
     languageOptions: {
-      sourceType: "commonjs"
+      ecmaVersion: 2021,
+      sourceType: "commonjs", // if you switch to ESM, change to 'module'
+      globals: {
+        ...globals.node,
+        ...globals.jest, // let tests use describe/it/expect without "no-undef"
+      },
     },
     rules: {
-      semi: "error",
-      quotes: ["error", "double"]
-    }
-  }
+      ...js.configs.recommended.rules,
+      // add project rules below (examples):
+      // "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      // "prefer-const": "warn",
+    },
+  },
 ];
